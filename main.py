3addfaf3eebf4b18835ffe4d7ec8b401df969d45
Jonathan Park <st4220@manukau.ac.nz>
2024-09-26 18:41:23 +1200

    Updates

--- main.py
+++ main.py
@@ -6,25 +6,26 @@ st.title("CrewAI!")
 st.markdown(
     """
     # Kia Ora!
-    Welcome to CrewAI Page!
+    Welcome to CrewAI Financial Assistance Page (Beta)!
+    This CrewAI Financial tool will help you to assess the financial needs but to also assess your consistency in the assessment.
 
-    👉 Here are the apps you can access from the Side bar👇:
+    👈Here are the apps you can access from the Side bar:
     """
 )
 
 # Navigation options
-st.markdown(
-    """
-    - [Online Form Crew](pages/online-form-crew.py)
-    - [Securedocs Crew](pages/securedoc-crew.py)
-    """
-)
+# st.markdown(
+#     """
+#     - [Online Form Crew](pages/online-form-crew.py)
+#     - [Securedocs Crew](pages/securedoc-crew.py)
+#     """
+# )
 
-# Optionally, add buttons for navigation
-if st.button("Go to Online Form Crew"):
-    st.experimental_set_query_params(page="Online Form Crew")
-    st.markdown("[Click here to go to Online Form Crew](pages/online-form-crew.py)")
+# # Optionally, add buttons for navigation
+# if st.button("Go to Online Form Crew"):
+#     st.experimental_set_query_params(page="Online Form Crew")
+#     st.markdown("[Click here to go to Online Form Crew](pages/online-form-crew.py)")
 
-if st.button("Go to Securedocs Crew"):
-    st.experimental_set_query_params(page="Securedocs Crew")
-    st.markdown("[Click here to go to Securedocs Crew](pages/securedoc-crew.py)")
+# if st.button("Go to Securedocs Crew"):
+#     st.experimental_set_query_params(page="Securedocs Crew")
+#     st.markdown("[Click here to go to Securedocs Crew](pages/securedoc-crew.py)")
