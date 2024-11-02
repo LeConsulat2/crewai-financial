e094b5aa16c626b569bfb901dffcf909e336f7b4
LeConsulat2 <wooine2@gmail.com>
2024-11-02 13:04:04 +1300

    Replaced Home.py

--- Home.py
+++ Home.py
@@ -1,26 +1,27 @@
 import streamlit as st
 
-st.title("CrewAI!")
+st.title("CrewAI Hello!")
 
 # Display welcome message
 st.markdown(
     """
     # Kia Ora!
-    Welcome to CrewAI Page!
+    Welcome to CrewAI Financial Assistance Page (Beta)!
+    This CrewAI Financial tool will help you to assess the financial needs but to also assess your consistency in the assessment.
 
-    👈Please access the CREWAI from the sidebar!:
+    👈Here are the apps you can access from the Side bar:
     """
 )
 
 # Navigation options
-st.markdown(
-    """
-    - [Online Form Crew](pages/online-form-crew.py)
-    - [Securedocs Crew](pages/securedoc-crew.py)
-    """
-)
+# st.markdown(
+#     """
+#     - [Online Form Crew](pages/online-form-crew.py)
+#     - [Securedocs Crew](pages/securedoc-crew.py)
+#     """
+# )
 
-# Optionally, add buttons for navigation
+# # Optionally, add buttons for navigation
 # if st.button("Go to Online Form Crew"):
 #     st.experimental_set_query_params(page="Online Form Crew")
 #     st.markdown("[Click here to go to Online Form Crew](pages/online-form-crew.py)")
